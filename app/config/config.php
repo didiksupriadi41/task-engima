416d9fc6aa8edeb0212a2771e2776f7680309695
Johanes <antoniusjohanes99@gmail.com>
2019-11-13 10:51:48 +0700

    change config to env

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,14 @@
 <?php
 
 // URL
-define('BASEURL', 'http://localhost/IF3110-2019-02-K03-03/engima/public/');
+// define('BASEURL', 'http://localhost/IF3110-2019-02-K03-03/engima/public/');
+define('BASEURL', getenv('BASEURL'));
 
 // DATABASE
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', getenv('USERNAME'));
+define('DB_PASS', getenv('PASSWORD'));
 define('DB_NAME', 'tubes2-wbd');
 
 // TheMovieDB
-define('API_KEY', getenv('API_KEY'));
+define('API_KEY', '079e3b7c50656338b9b09fc893c231bb');
