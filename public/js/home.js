ff4026779ed2c6bb6fba1df5db618e04183755a0
Johanes <antoniusjohanes99@gmail.com>
2019-11-20 17:52:38 +0700

    grid view on home page

--- public/js/home.js
+++ public/js/home.js
@@ -9,27 +9,26 @@ var btnWrapper = document.getElementById("pagination")
 var loc = window.location.pathname;
 loc = loc.split("/");
 var dir = loc.slice(0, loc.lastIndexOf("public") + 1).join("/");
+var pagination = 5;
 
 function formatMovieHtml(result, index) {
     var poster = (result[index].poster == null) ?
-        '<i class="no_image_holder"></i>' :
+        `<img class="home-poster" src="${dir}/img/no_img_placeholder.jpg">` :
         `<img class="home-poster" src="${result[index].poster}">`;
     return `
-        <div class="col-2">
+        <div class="movie-home-wrapper">
             <a class="movie-link" href="${dir}/movie/detail?id=${result[index].idMovie}">
-                <div class="movie-home-wrapper">
-                    <div class="home-poster-wrapper">
-                        ${poster}
-                    </div>
-                    <div class="home-movie-title">
-                        ${result[index].title}
-                    </div>
-                    <div class="home-rating-wrapper">
-                        <img class="home-star" src="${dir}/img/star.png">
-                        <span class="home-rating">
-                            ${result[index].rating}
-                        </span>
-                    </div>
+                <div class="home-poster-wrapper">
+                    ${poster}
+                </div>
+                <div class="home-movie-title">
+                    ${result[index].title}
+                </div>
+                <div class="home-rating-wrapper">
+                    <img class="home-star" src="${dir}/img/star.png">
+                    <span class="home-rating">
+                        ${result[index].rating}
+                    </span>
                 </div>
             </a>
         </div>`;
@@ -42,17 +41,16 @@ function addMovie(page, parent, xhr) {
     var rowCount = Math.ceil(result.length / nCol);
     var index = 0;
     for (let row = 0; row < rowCount; row++) {
-        holder += '<div class="row row-movie">';
+        // holder += '<div class="row row-movie">';
         for (var col = 0; col < nCol && index < result.length; col++) {
             holder += formatMovieHtml(result, index);
             index++;
         }
-        holder += '</div>';
+        // holder += '</div>';
     }
-    console.log(holder);
+    // console.log(holder);
 
     parent.innerHTML = holder;
-    var pagination = 5;
     if (parseInt(pageCount, 10) < pagination) {
         pagination = parseInt(pageCount, 10);
     }
@@ -104,6 +102,8 @@ nextButton.addEventListener("click", function () {
     var page = inputPage.value
     inputPage.value = parseInt(page, 10) + 1
     ajax(inputPage.value, mainMovieWrapper);
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
     if (parseInt(page, 10) + 1 == parseInt(pageCount, 10)) {
         nextButton.disabled = true;
     }
@@ -116,6 +116,8 @@ prevButton.addEventListener("click", function () {
     var page = inputPage.value
     inputPage.value = parseInt(page, 10) - 1
     ajax(inputPage.value, mainMovieWrapper);
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
     if (parseInt(page, 10) + 1 != parseInt(pageCount, 10)) {
         nextButton.disabled = false;
     }
@@ -129,6 +131,8 @@ for (var i = 0; i < pageButton.length; i++) {
         var page = parseInt(this.innerHTML, 10);
         inputPage.value = page;
         ajax(inputPage.value, mainMovieWrapper);
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
         if (parseInt(page, 10) == parseInt(pageCount, 10)) {
             nextButton.disabled = true;
         } else {
@@ -146,11 +150,29 @@ window.onload = function () {
     if (pageCount < 2) {
         btnWrapper.style.display = "none";
     } else {
-        prevButton.disabled = true;
-        pageButton[0].disabled = true;
-        if (pageCount == 1) {
-            nextButton.disabled = true;
+        ajax(inputPage.value, mainMovieWrapper);
+        var page = parseInt(inputPage.value, 10);
+        // var i = pagination;
+        if (page == 1) {
+            prevButton.disabled = true;
+            //     //     pageButton[0].disabled = true;
+            if (pageCount == 1) {
+                nextButton.disabled = true;
+            }
         }
-
+        // else {
+        //     while (i > 0) {
+        //         pageButton[pagination - i].innerHTML = parseInt(page, 10) - i + 1
+        //         pageButton[pagination - i].disabled = false;
+        //         if ((parseInt(pageCount, 10) - i + 1) == page) {
+        //             pageButton[pagination - i].disabled = true;
+        //         }
+        //         i -= 1;
+        //     }
+        //     //     pageButton[page - 1].disabled = true;
+        //     if (page == parseInt(pageCount, 10)) {
+        //         nextButton.disabled = true;
+        //     }
+        // }
     }
 };
